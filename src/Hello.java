fff4cb1e1f373ce7445124811db0e566fc31d4d6
lukeu <lukeu@ChekhovsGun.unl.edu>
2024-08-30 16:38:47 -0500

    Adding My name and major

--- src/Hello.java
+++ src/Hello.java
@@ -7,6 +7,9 @@ public class Hello {
 
   public static void main(String args[]) {
     System.out.println("Keenan Flannery");
+    System.out.println("Computer Science");
+    System.out.println("Luke Uhlir");
+    System.out.println("Computer Engineering");
   }
 
 }
